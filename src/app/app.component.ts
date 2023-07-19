0dd94455b444f6f8999453783d7bf525b937b700
Karen Ale <karen_ale05@hotmail.com>
2023-07-18 23:23:04 -0300

    Se realizó la tarea asignada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Producto } from './interfaces/producto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-proyect';
+
+  lista:Producto[] = [
+    {
+      nombre: "Pure",
+      costo: "$60"
+    },
+    {
+      nombre: "Tomate",
+      costo:"$50"
+
+    },
+    {
+      nombre: "Carne",
+      costo:"$1500"
+
+    },{
+      nombre: "Yerba",
+      costo:"$500"
+
+    },
+  ]
+
+  listaVisible:boolean = true;
+
+  textoBoton:string = "Ocultar"
+
+  botonPresionado() {
+    this.listaVisible = !this.listaVisible ;
+
+    if(this.listaVisible == false ){
+    this.textoBoton = "Mostrar lista"
+    }
+    else{
+      this.textoBoton = "Ocultar"
+    }
+  }
+
 }
